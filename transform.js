79fca57a36264a412da505b4b8b2ea8a7a7968b0
wwl <wwl_github@163.com>
2018-04-10 00:46:15 +0800

    修改transform.js fixbug: 如果打包为commonjs，则全局作用域的变量不会压缩。

--- transform.js
+++ transform.js
@@ -1,27 +1,31 @@
 var babel = require("babel-core");
 var fs = require('fs');
 var UglifyJS = require("uglify-es");
+// var UglifyJS = require("uglify-js");
 
 
 var file = __dirname + '/index.js';
 var babelResult = babel.transformFileSync(file,
     {
         presets: [
-            ['env', {modules: 'commonjs'}]
-        ]
+            ['env', {modules: 'umd'}]
+        ],
+        moduleId: 'dom'
     }
 );
 
 var code = babelResult.code;
 
+// console.log(code);
+
 var minifyResult = UglifyJS.minify(code, {
     compress: {
         drop_console: true
     },
-    output: {
-        ascii_only: true,
-    },
-    ecma: 5
+    ecma: 7,
+    parse:{
+        bare_returns:true
+    }
 });
 code = minifyResult.code;
 
@@ -29,4 +33,5 @@ code = minifyResult.code;
 
 var distFile = __dirname + '/dist/index.js';
 fs.writeFileSync(distFile, code, 'utf8');
+
 
